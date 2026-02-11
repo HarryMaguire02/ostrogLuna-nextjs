8f11f9bff1496ce958e7ac6279e6afb9eb4cd9e7
Aleksa Kozic <kozicaleksa02@gmail.com>
2026-02-11 22:18:44 +0100

    Footer implementation - Removed default icons - Updated padding on container component - Updated styling for footer component and use of our logo svgs

--- ostrog-luna-nextjs/app/components/Container.tsx
+++ ostrog-luna-nextjs/app/components/Container.tsx
@@ -1,19 +1,15 @@
 interface ContainerProps {
   children: React.ReactNode;
   className?: string;
-  noPaddingMobile?: boolean;
 }
 
 export default function Container({
   children,
   className = "",
-  noPaddingMobile = false,
 }: ContainerProps) {
   return (
     <div
-      className={`max-w-7xl mx-auto ${
-        noPaddingMobile ? "px-0 sm:px-6 lg:px-8" : "px-4 sm:px-6 lg:px-8"
-      } ${className}`}
+      className={`max-w-7xl mx-auto px-4 ${className}`}
     >
       {children}
     </div>
